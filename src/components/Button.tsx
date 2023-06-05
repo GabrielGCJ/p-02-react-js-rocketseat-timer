16a3be11928a07e76fefc52f57db022a238cff5a
Gabriel Jorge <gabriel.ggcj@gmail.com>
2023-06-05 09:45:47 -0300

    feature/add-react-router-dom

--- src/components/Button.tsx
+++
@@ -1,9 +0,0 @@
-import { ButtonContainer, ButtonVariants } from './ButtonStyles'
-
-interface ButtonProps {
-  variant?: ButtonVariants
-}
-
-export const Button = ({ variant = 'primary' }: ButtonProps) => {
-  return <ButtonContainer variant={variant}>Enviar</ButtonContainer>
-}
