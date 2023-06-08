418e69bf3103f8d1b64be2744416a16900a380e7
gabrielj <gabriel.ggcj@gmail.com>
2023-06-08 00:22:55 -0300

    feature/new-update-history-page

--- src/pages/History/styles.ts
+++ src/pages/History/styles.ts
@@ -2,7 +2,7 @@ import styled from 'styled-components'
 
 export const HistoryContainer = styled.main`
   flex: 1;
-  padding: 3.5rem;
+  /* padding: 3.5rem; */
 
   display: flex;
   flex-direction: column;
